b44478276025b8b45233d6c9aec7388f07f725fc
Subbu <svenkat683@gmail.com>
2025-08-24 11:40:46 +0530

    refactor: remove redundant background script files

--- entrypoints/content.ts
+++
@@ -1,9 +0,0 @@
-export default defineContentScript({
-  matches: [
-    'https://github.com/*',
-    'https://*.github.com/*'
-  ],
-  main() {
-    console.log('FirstCommit content script loaded on GitHub.');
-  },
-});
